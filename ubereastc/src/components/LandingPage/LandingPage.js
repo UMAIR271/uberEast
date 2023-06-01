3b0c591f3a3fdc52260f1eb33cc8379b83ea7a42
Anand Pandey <anand2.pandey@paytm.com>
2023-06-01 13:09:40 +0530

    Adding Styling to it

--- ubereastc/src/components/LandingPage/LandingPage.js
+++ ubereastc/src/components/LandingPage/LandingPage.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import IconButton from "@mui/material/IconButton";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import axios from "axios";
 import debounce from "lodash.debounce";
+import "./LandingPage.css";
 
 export default function LandingPage() {
   const [searchText, setSearchText] = useState("");
@@ -32,92 +34,86 @@ export default function LandingPage() {
         maxWidth: "1440px",
         minHeight: "100vh",
         margin: "0 auto",
-        overflowX: "hidden",
       }}
     >
-      {/* Header */}
-      <div
-        style={{
-          position: "absolute",
-          top: "20px",
-          left: "20px",
-          color: "#fff",
-        }}
-      >
-        <h1 style={{ fontSize: "28px", marginBottom: "8px" }}>
-          Zeit, Essen zu bestellen
-        </h1>
-        <h3 style={{ fontSize: "16px" }}>
-          Jetzt ein diazo in Deiner Nähe finden
-        </h3>
-      </div>
-
       {/* Left Part */}
       <div
         className="leftPart"
         style={{
           position: "absolute",
-          top: "60px",
+          top: 0,
           left: 0,
-          width: "60%",
-          height: "100%",
-          backgroundColor: "#FFD600",
+          bottom: 0,
+          width: "70%",
           display: "flex",
           alignItems: "center",
-          justifyContent: "center",
-          paddingTop: "60px", // Added padding-top to push contents down
+          justifyContent: "flex-start",
+          paddingTop: "60px",
+          overflow: "hidden",
         }}
       >
-        <div
+        <section
           style={{
-            display: "flex",
-            alignItems: "center",
-            width: "80%",
-            border: "1px solid #000",
-            borderRadius: "20px",
-            padding: "4px",
+            position: "relative",
+            width: "60%",
+            marginLeft: "10%",
+            textAlign:"left"
           }}
         >
-          <IconButton
-            color="primary"
-            aria-label="menu"
-            style={{ padding: "8px" }}
-          >
-            <LocationOnIcon /> {/* Render the menu icon */}
-          </IconButton>
-          <input
-            type="text"
-            placeholder="Search City"
-            style={{
-              width: "calc(100% - 88px)", // Adjusted width considering the menu icon
-              height: "100%",
-              padding: "8px",
-              fontSize: "16px",
-              border: "none",
-              outline: "none",
-            }}
-            onChange={(e) => handleSearchChange(e.target.value)}
-          />
-          <button
-            type="button"
+            <h1 className="header1"> Zeit, Essen zu bestellen</h1>
+            <h3 className="header2">Jetzt ein diazo in Deiner Nähe finden</h3>
+          <div
             style={{
               display: "flex",
               alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "transparent",
-              border: "none",
-              outline: "none",
-              padding: "8px",
+
+              border: "1px solid #000",
+              borderRadius: "20px",
+              padding: "4px",
+              backgroundColor: "white",
             }}
           >
-            <SearchIcon style={{ color: "#000" }} />
-          </button>
-        </div>
-        <ul>
-        {cityResults.map((result) => (
-          <li key={result.place_id}>{result.description}</li>
-        ))}
-      </ul>
+            <IconButton
+              color="primary"
+              aria-label="menu"
+              style={{ padding: "8px" }}
+            >
+              <LocationOnIcon /> {/* Render the menu icon */}
+            </IconButton>
+            <input
+              type="text"
+              placeholder="Search City"
+              style={{
+                width: "calc(100% - 88px)", // Adjusted width considering the menu icon
+                height: "100%",
+                padding: "8px",
+                fontSize: "16px",
+                border: "none",
+                outline: "none",
+              }}
+              onChange={(e) => handleSearchChange(e.target.value)}
+            />
+            <button
+              type="button"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "transparent",
+                border: "none",
+                outline: "none",
+                padding: "8px",
+              }}
+            >
+              <SearchIcon style={{ color: "#000" }} />
+            </button>
+          </div>
+          <ul>
+            {cityResults.map((result) => (
+              <li key={result.place_id}>{result.description}</li>
+            ))}
+          </ul>
+        </section>
       </div>
 
       {/* Right Part */}
@@ -127,8 +123,8 @@ export default function LandingPage() {
           position: "absolute",
           top: 0,
           right: 0,
-          width: "40%",
-          height: "100%",
+          bottom: 0,
+          width: "30%",
           backgroundColor: "white",
         }}
       >
